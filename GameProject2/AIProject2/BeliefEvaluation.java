a15ce28a77aa43d896e1c136c0c9305516ca6ef5
VladTsimoshchanka <78988772+VladTsimoshchanka@users.noreply.github.com>
2021-03-18 19:22:02 -0400

    Crude belief evaluation
    Will update it later and add comments

---
+++ GameProject2/AIProject2/BeliefEvaluation.java
@@ -0,0 +1,75 @@
+package GameProject2.AIProject2;
+import ProjectTwoEngine.*;
+public class BeliefEvaluation{
+public List<Float> ReevaluateBelief(GameState gs){
+GameState gamestate = gs;
+
+List<Float> Beliefs = new List<Float>(3);
+    if(gs.isTopTurn()){
+        for(int i = 0; i < 3; i++){
+            List Evilmonsters = gs.getMonsters(i, PlayerID.BOT);
+            int EvilProwess = 0;
+            for(Monster m: Evilmonsters){
+                EvilProwess+=m.value;
+            }
+            List Fairmonsters = gs.getMonsters(i, PlayerID.TOP);
+            int FairProwess = 0;
+            for(Monster m: Fairmonsters){
+                FairProwess+=m.value;
+            }
+            if(FairProwess>EvilProwess){
+                if(FairProwess>EvilProwess+6){
+                    Beliefs[i] = 1.0;
+                }
+                else{
+                    Beliefs[i] = 0.5;
+                }
+            }
+            if(FairProwess<EvilProwess){
+                if(FairProwess+6<EvilProwess && gamestate.getHidden(PlayerID.TOP) != i){
+                    Beliefs[i] = 0.0;
+                }
+                else{
+                    Beliefs[i] = 0.5; 
+                }
+            }
+            
+
+    }
+    }
+    else(){
+        for(int i = 0; i < 3; i++){
+            List Evilmonsters = gs.getMonsters(i, PlayerID.Top);
+            int EvilProwess = 0;
+            for(Monster m: Evilmonsters){
+                EvilProwess+=m.value;
+            }
+            List Fairmonsters = gs.getMonsters(i, PlayerID.BOT);
+            int FairProwess = 0;
+            for(Monster m: Fairmonsters){
+                FairProwess+=m.value;
+            }
+            if(FairProwess>EvilProwess){
+                if(FairProwess>EvilProwess+6){
+                    Beliefs[i] = 1.0;
+                }
+                else{
+                    Beliefs[i] = 0.5;
+                }
+            }
+            if(FairProwess<EvilProwess){
+                if(FairProwess+6<EvilProwess && gamestate.getHidden(PlayerID.BOT) != i){
+                    Beliefs[i] = 0.0;
+                }
+                else{
+                    Beliefs[i] = 0.5; 
+                }
+            }
+            
+
+    }
+    }
+return Beliefs;
+}
+
+}
