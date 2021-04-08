f706c2a2e1646f754b4011f08169c03ef95353ed
VladTsimoshchanka <78988772+VladTsimoshchanka@users.noreply.github.com>
2021-04-08 16:26:35 -0400

    The fix

--- GameProject2/AIProject2/BeliefEvaluation.java
+++ GameProject2/AIProject2/BeliefEvaluation.java
@@ -1,75 +1,59 @@
 package AIProject2;
+import java.util.ArrayList;
+import java.util.List;
+
 import ProjectTwoEngine.*;
 public class BeliefEvaluation{
-public List<Float> ReevaluateBelief(GameState gs){
+public List<Float> ReevaluateBelief(ArrayList<Float> beliefs, GameState gs, PlayerID player){
 GameState gamestate = gs;
-
-List<Float> Beliefs = new List<Float>(3);
-    if(gs.isTopTurn()){
-        for(int i = 0; i < 3; i++){
-            List Evilmonsters = gs.getMonsters(i, PlayerID.BOT);
+CastleID castle = CastleID.CastleA;
+List<Float> Beliefs = beliefs;
+ArrayList OpponentForces = new ArrayList<Integer>();
+    for(int i = 0; i<3; i++){
+        switch(i){
+            case 0:
+                castle=CastleID.CastleA;
+                break;
+            case 1:
+                castle=CastleID.CastleB;
+                break;
+            case 2:
+                castle=CastleID.CastleC;
+                break;
+        }
+        if(player == PlayerID.TOP){
+            List<Monster> Evilmonsters = gs.getMonsters(castle, PlayerID.BOT);
             int EvilProwess = 0;
             for(Monster m: Evilmonsters){
                 EvilProwess+=m.value;
             }
-            List Fairmonsters = gs.getMonsters(i, PlayerID.TOP);
-            int FairProwess = 0;
-            for(Monster m: Fairmonsters){
-                FairProwess+=m.value;
-            }
-            if(FairProwess>EvilProwess){
-                if(FairProwess>EvilProwess+6){
-                    Beliefs[i] = 1.0;
-                }
-                else{
-                    Beliefs[i] = 0.5;
-                }
-            }
-            if(FairProwess<EvilProwess){
-                if(FairProwess+6<EvilProwess && gamestate.getHidden(PlayerID.TOP) != i){
-                    Beliefs[i] = 0.0;
-                }
-                else{
-                    Beliefs[i] = 0.5; 
-                }
-            }
+            OpponentForces.add(EvilProwess);
             
-
-    }
-    }
-    else{
-        for(int i = 0; i < 3; i++){
-            List Evilmonsters = gs.getMonsters(i, PlayerID.Top);
+            
+        }
+        else if(player == PlayerID.BOT){
+            List<Monster> Evilmonsters = gs.getMonsters(castle, PlayerID.TOP);
             int EvilProwess = 0;
             for(Monster m: Evilmonsters){
                 EvilProwess+=m.value;
             }
-            List Fairmonsters = gs.getMonsters(i, PlayerID.BOT);
-            int FairProwess = 0;
-            for(Monster m: Fairmonsters){
-                FairProwess+=m.value;
-            }
-            if(FairProwess>EvilProwess){
-                if(FairProwess>EvilProwess+6){
-                    Beliefs[i] = 1.0;
-                }
-                else{
-                    Beliefs[i] = 0.5;
-                }
-            }
-            if(FairProwess<EvilProwess){
-                if(FairProwess+6<EvilProwess && gamestate.getHidden(PlayerID.BOT) != i){
-                    Beliefs[i] = 0.0;
-                }
-                else{
-                    Beliefs[i] = 0.5; 
-                }
-            }
-            
+            OpponentForces.add(EvilProwess);
+        }
 
     }
-    }
+Beliefs = makeEvaluation(OpponentForces);
 return Beliefs;
 }
-
+public List<Float> makeEvaluation(ArrayList<Integer> Forces){
+    int AllForcesSummed = 0;
+    for(int force: Forces){
+        AllForcesSummed+=force;
+    }
+    List<Float> NewBeliefs = new ArrayList<Float>();
+    for(int i = 0; i<3; i++){
+        NewBeliefs.set(i, (float) NewBeliefs.get(i)/AllForcesSummed);
+        NewBeliefs.set(i, 1-NewBeliefs.get(i));
+    }
+    return NewBeliefs;
+}
 }
